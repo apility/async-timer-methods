d27f083dd5fbe8ad98459b060dbe2472bc0d91cf
Cathrine Vaage <cathrine.vaage@gmail.com>
2018-07-31 15:23:17 +0200

    Removes asyncSetInterval

--- src/asyncSetInterval.js
+++
@@ -1,9 +0,0 @@
-export default (delay, returnIntervalID = false, ...params) => {
-  let intervalID = undefined
-
-  const promise = new Promise(resolve => {
-    intervalID = window.setInterval(resolve, delay, params)
-  })
-
-  return returnIntervalID ? [promise, intervalID] : promise
-}
