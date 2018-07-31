33e94191e388e2006f2757d1143593ff484e2c94
Johan Vaage <jovaage@gmail.com>
2018-07-31 15:11:47 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as asyncSetTimeout } from './asyncSetTimeout'
+export { default as asyncSetInterval } from './asyncSetInterval'
+export { default as asyncSetImmediate } from './asyncSetImmediate'
