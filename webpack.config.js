dd95097694461d3c99baa155f90583d7fab157c8
Cathrine Vaage <cathrine.vaage@gmail.com>
2018-12-28 12:46:49 +0100

    Adds actual transpilation through babel this time

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,17 @@ const path = require('path')
 module.exports = {
   entry: './src/index.js',
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'asyncTimerMethods.js',
